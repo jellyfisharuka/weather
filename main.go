98beba6d4c3c815b7fe7dd336814e9f583e41ca1
jellyfisharuka <arukeulen@gmail.com>
2024-02-13 00:09:39 +0600

    load api, def structs

--- main.go
+++ main.go
@@ -1,4 +1,30 @@
 package main
-func main() {
-type ApiConfig
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type apiConfigData struct {
+	OpenWeatherMapApiKey string `json:"OpenWeatherMapApiLey"`
+}
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
+
+func loadApiConfig(filename string) (apiConfigData, error) {
+	bytes, err := os.ReadFile(filename)
+	if err!=nil {
+		return apiConfigData{}, err
+	}
+	var c apiConfigData
+	err=json.Unmarshal(bytes, &c)
+	if err!=nil {
+		return apiConfigData{}, err
+	}
+	return c,nil
 }
