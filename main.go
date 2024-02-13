cd32dc4dfa8183192914ad4bdb788be24c165b6e
jellyfisharuka <arukeulen@gmail.com>
2024-02-14 01:39:30 +0600

    handle func, http responses

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"net/http"
 	"os"
+	"strings"
 )
 
 type apiConfigData struct {
@@ -27,4 +29,20 @@ func loadApiConfig(filename string) (apiConfigData, error) {
 		return apiConfigData{}, err
 	}
 	return c,nil
+}
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/weather/", 
+	func(w http.ResponseWriter, r *http.Request){
+		city:= strings.SplitN(r.URL.Path, "/", 3)[2]
+		data,err:=query(city)
+		if err!=nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return 
+		}
+		w.Header().Set("content-type", "application/json;charset=utf-8")
+		json.NewEncoder(w).Encode(data)
+
+	})
+	http.ListenAndServe(":8080", nil)
 }
