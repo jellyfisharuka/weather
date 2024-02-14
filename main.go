946b012f3e2b59383a5f36a2dfeb1880eedfa2fb
jellyfisharuka <arukeulen@gmail.com>
2024-02-15 00:33:00 +0600

    html template

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 	"strings"
 	"weather/routes"
-
 )
+
 
 
 func main() {
--- routes/hello.go
+++ routes/hello.go
@@ -1,7 +1,28 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+	"text/template"
+)
 
-func Hello(w http.ResponseWriter, r*http.Request) {
+var (
+	tmpl = template.Must(template.ParseFiles("C:/Users/aruke/OneDrive/Рабочий стол/weather/routes/template/template.html"))
+)
+type ContactDetail struct {
+	Password string
+	Success bool
+	City string
+	StorageAccess string
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from go!\n"))
+	w.Header().Set("Content-Type", "text/html") 
+	http.ServeFile(w, r, "C:/Users/aruke/OneDrive/Рабочий стол/weather/routes/template/template.html")
+
+	data := ContactDetail{
+		City:     r.FormValue("city"),
+		Password: r.FormValue("password"),
+	}
+	tmpl.Execute(w, data)
 }
