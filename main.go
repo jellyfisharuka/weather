ac23e23604b0f38aef9288c0f24cb5eb98c792cc
jellyfisharuka <arukeulen@gmail.com>
2024-02-15 13:45:19 +0600

    simple template for test

--- main.go
+++ main.go
@@ -7,21 +7,20 @@ import (
 	"weather/routes"
 )
 
-
-
 func main() {
 	http.HandleFunc("/hello", routes.Hello)
-	http.HandleFunc("/weather/", 
-	func(w http.ResponseWriter, r *http.Request){
-		city:= strings.SplitN(r.URL.Path, "/", 3)[2]
-		data,err:=routes.Query(city)
-		if err!=nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return 
-		}
-		w.Header().Set("content-type", "application/json;charset=utf-8")
-		json.NewEncoder(w).Encode(data)
+	http.HandleFunc("/weather/",
+		func(w http.ResponseWriter, r *http.Request) {
+			city := strings.SplitN(r.URL.Path, "/", 3)[2]
+			data, err := routes.Query(city)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("content-type", "application/json;charset=utf-8")
+			json.NewEncoder(w).Encode(data)
 
-	})
+		})
+	http.HandleFunc("/test", routes.Test)
 	http.ListenAndServe(":8080", nil)
 }
--- routes/hello.go
+++ routes/hello.go
@@ -1,28 +1,40 @@
 package routes
 
 import (
+	"html/template"
 	"net/http"
-	"text/template"
 )
 
 var (
-	tmpl = template.Must(template.ParseFiles("C:/Users/aruke/OneDrive/Рабочий стол/weather/routes/template/template.html"))
+	tmpl = template.Must(template.ParseFiles("template/template.html"))
 )
+
 type ContactDetail struct {
-	Password string
-	Success bool
-	City string
+	Password      string
+	Success       bool
+	City          string
 	StorageAccess string
+}
+type ViewData struct {
+	Title   string
+	Message string
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from go!\n"))
-	w.Header().Set("Content-Type", "text/html") 
-	http.ServeFile(w, r, "C:/Users/aruke/OneDrive/Рабочий стол/weather/routes/template/template.html")
+	w.Header().Set("Content-Type", "html/html")
 
 	data := ContactDetail{
 		City:     r.FormValue("city"),
 		Password: r.FormValue("password"),
 	}
 	tmpl.Execute(w, data)
+}
+func Test(w http.ResponseWriter, r *http.Request) {
+	testData := ViewData{
+		Title:   "word",
+		Message: "message",
+	}
+	tmpl, _ := template.ParseFiles("template/test.html")
+	tmpl.Execute(w, testData)
 }
