1eba4a1d9d6f3212f61d702824438c5bc549cf5d
jellyfisharuka <arukeulen@gmail.com>
2024-03-27 14:40:02 +0500

    gitlab

--- routes/hello.go
+++ routes/hello.go
@@ -6,35 +6,39 @@ import (
 )
 
 var (
-	tmpl = template.Must(template.ParseFiles("template/template.html"))
+	tmpl1, _ = template.ParseFiles("template/template.html")
+	tmpl2, _ = template.ParseFiles("template/test.html")
 )
 
 type ContactDetail struct {
 	Password      string
-	Success       bool
 	City          string
+	Success       bool
 	StorageAccess string
 }
 type ViewData struct {
 	Title   string
 	Message string
+	T string
+	Mes string
+	Success bool
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from go!\n"))
-	w.Header().Set("Content-Type", "html/html")
-
 	data := ContactDetail{
-		City:     r.FormValue("city"),
 		Password: r.FormValue("password"),
+		City:     r.FormValue("city"),
 	}
-	tmpl.Execute(w, data)
+	//data.Success = true
+	tmpl1.Execute(w, data)
 }
 func Test(w http.ResponseWriter, r *http.Request) {
 	testData := ViewData{
 		Title:   "word",
 		Message: "message",
+		T: "ttt",
+		Mes: "mess",
 	}
-	tmpl, _ := template.ParseFiles("template/test.html")
-	tmpl.Execute(w, testData)
+   testData.Success=false
+	tmpl2.Execute(w, testData)
 }
